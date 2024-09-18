63d70ba933efb41c3177ea194f3852e1dad2286f
unknown <laurenceherrera90@gmail.com>
2024-09-18 14:39:19 +0800

    first upload

---
+++ hello.py
@@ -0,0 +1,9 @@
+'''
+1. Print Hello World
+2. Put firstname , lastname in a variable
+3. Display your fullname "Hello, my name is fullname" 
+'''
+print("Hello World")
+firstn = "Ralph Andre Laurence"
+lastn = "Herrera"
+print("Hello, my name is " + firstn + " " + lastn)
